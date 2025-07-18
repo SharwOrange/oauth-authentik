ecb2bd0b593571e21e7833589a5321c0023d71c0
sharworange <kuliangcha@qq.com>
2025-07-18 22:48:05 +0800

    不会影响皮肤站的运行，但无法登录

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-# plugin-template
+# 使用 Authentik 登录
+当您想使用 Authentik 登录皮肤站时，可以使用本插件能让现有用户进行这样的操作。
 
-This is a template repository for creating plugin, feel free to use it then modify it.
+## 使用方法
 
-## License
+本插件没有配置页面，所有配置通过修改 `.env` 来进行。
 
-MIT License
+1. 在 你的Authentik 创建 OAuth 2 应用
+2. 增加三条配置项， `AUTHENTIK_BASE_URL`、 `AUTHENTIK_CLIENT_ID`、 `AUTHENTIK_CLIENT_SECRE`、 `AUTHENTIK_REDIRECT_URI`
+3. 将  `你Authentik的地址`、  `客户端 ID`、  `客户端 Secret`、  `重定向 URI` 分别填入 `AUTHENTIK_BASE_URL`、 `AUTHENTIK_CLIENT_ID`、 `AUTHENTIK_CLIENT_SECRE`、 `AUTHENTIK_REDIRECT_URI`
+
+## 示例
+
+```
+AUTHENTIK_BASE_URL=https://authentik.company/
+AUTHENTIK_CLIENT_ID=1
+AUTHENTIK_CLIENT_SECRE=xxxxx
+AUTHENTIK_REDIRECT_URI=https://skin.blessingskin/
+```
--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,27 @@
 
 use Blessing\Filter;
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Support\Collection;
+use SocialiteProviders\Manager\SocialiteWasCalled;
+use SocialiteProviders\Authentik\Provider;
 
 return function (Dispatcher $events, Filter $filter) {
-    //
+    $events->listen(SocialiteWasCalled::class, function (SocialiteWasCalled $event) {
+        $event->extendSocialite('authentik', Provider::class);
+    });
+
+    config(['services.authentik' => [
+        'base_url' => env('AUTHENTIK_BASE_URL'),
+        'client_id' => env('AUTHENTIK_CLIENT_ID'),
+        'client_secret' => env('AUTHENTIK_CLIENT_SECRET'),
+        'redirect' => env('AUTHENTIK_REDIRECT_URI')
+    ]]);
+
+    $filter->add('oauth_providers', function (Collection $providers) {
+        $providers->put('authentik', [
+            'icon' => 'users',
+            'displayName' => 'Authentik',
+        ]);
+        return $providers;
+    });
 };
